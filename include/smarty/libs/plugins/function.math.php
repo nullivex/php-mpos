22b8370f358a19fd2344f651a89f933aaf369d69
Bryan Tong <contact@nullivex.com>
2018-01-20 15:58:31 -0700

    add new version of smarty

--- include/smarty/libs/plugins/function.math.php
+++ include/smarty/libs/plugins/function.math.php
@@ -18,11 +18,11 @@
  * @author   Monte Ohrt <monte at ohrt dot com>
  *
  * @param array                    $params   parameters
- * @param Smarty
+ * @param Smarty_Internal_Template $template template object
  *
  * @return string|null
  */
-function smarty_function_math($params, &$smarty)
+function smarty_function_math($params, $template)
 {
     static $_allowed_funcs =
         array('int' => true, 'abs' => true, 'ceil' => true, 'cos' => true, 'exp' => true, 'floor' => true,
@@ -58,28 +58,12 @@ function smarty_function_math($params, &$smarty)
         return;
     }
 
-    foreach ($params as $key => $val) {
-        if ($key != "equation" && $key != "format" && $key != "assign") {
-            // make sure value is not empty
-            if (strlen($val) == 0) {
-                trigger_error("math: parameter '{$key}' is empty", E_USER_WARNING);
-
-                return;
-            }
-            if (!is_numeric($val)) {
-                trigger_error("math: parameter '{$key}' is not numeric", E_USER_WARNING);
-
-                return;
-            }
-        }
-    }
-
     // match all vars in equation, make sure all are passed
     preg_match_all('!(?:0x[a-fA-F0-9]+)|([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)!', $equation, $match);
 
     foreach ($match[ 1 ] as $curr_var) {
         if ($curr_var && !isset($params[ $curr_var ]) && !isset($_allowed_funcs[ $curr_var ])) {
-            trigger_error("math: function call '{$curr_var}' not allowed, or missing parameter '{$curr_var}'", E_USER_WARNING);
+            trigger_error("math: function call $curr_var not allowed", E_USER_WARNING);
 
             return;
         }
@@ -87,6 +71,17 @@ function smarty_function_math($params, &$smarty)
 
     foreach ($params as $key => $val) {
         if ($key != "equation" && $key != "format" && $key != "assign") {
+            // make sure value is not empty
+            if (strlen($val) == 0) {
+                trigger_error("math: parameter $key is empty", E_USER_WARNING);
+
+                return;
+            }
+            if (!is_numeric($val)) {
+                trigger_error("math: parameter $key: is not numeric", E_USER_WARNING);
+
+                return;
+            }
             $equation = preg_replace("/\b$key\b/", " \$params['$key'] ", $equation);
         }
     }
@@ -97,13 +92,13 @@ function smarty_function_math($params, &$smarty)
         if (empty($params[ 'assign' ])) {
             return $smarty_math_result;
         } else {
-            $smarty->assign($params[ 'assign' ], $smarty_math_result);
+            $template->assign($params[ 'assign' ], $smarty_math_result);
         }
     } else {
         if (empty($params[ 'assign' ])) {
             printf($params[ 'format' ], $smarty_math_result);
         } else {
-            $smarty->assign($params[ 'assign' ], sprintf($params[ 'format' ], $smarty_math_result));
+            $template->assign($params[ 'assign' ], sprintf($params[ 'format' ], $smarty_math_result));
         }
     }
 }
